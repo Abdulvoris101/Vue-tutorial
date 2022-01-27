3b2fb6fec7db01f4aafc48e6a0e143e2233a354f
Abdulvoris101 <erkinovabdulvoris101@gmail.com>
2022-01-27 20:31:20 +0500

    Photo Refactoring and learning using style class

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
-  <FindUser />
+  <PhotoRedactor />
 </template>
 
 <script>
-import FindUser from '@/components/FindUser' 
+// import FindUser from '@/components/FindUser' 
 // homework
 // import userCard from '@/components/userCard'
-
+import PhotoRedactor from '@/components/PhotoRedactor'
 export default {
   name: 'App',
   components: {
-    FindUser
+    PhotoRedactor
   }
 }
 </script>
