4035c7ce1b235d7a10f41b45e5f31db942580d2f
Abdulvoris101 <erkinovabdulvoris101@gmail.com>
2022-01-27 19:31:50 +0500

    delete 3-lesson

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,13 @@
 <template>
-  <FindUser />
+  <PhotoRedactor />
 </template>
 
 <script>
-import FindUser from '@/components/FindUser' 
-// homework
-// import userCard from '@/components/userCard'
-
+import PhotoRedactor from '@/components/PhotoRedactor'
 export default {
   name: 'App',
   components: {
-    FindUser
+    PhotoRedactor
   }
 }
 </script>
